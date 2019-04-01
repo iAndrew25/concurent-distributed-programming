9a379dd273056a94ece8b380977ef951338d5589
Andy <andy@192.168.1.7>
2019-04-01 18:20:40 +0300

    Delete All, Put All

--- rest-api/server.js
+++ rest-api/server.js
@@ -13,7 +13,7 @@ let persons = [{
 }, {
 	ssn: 1941029,
 	name: 'Cosmin',
-	age: 24
+	age: 23
 }, {
 	ssn: 2950611,
 	name: 'Diana',
@@ -83,6 +83,36 @@ app.put('/persons/:ssn', (req, res) => {
 	}
 });
 
+
+app.put('/persons/all/:age', (req, res) => {
+	try {
+		let age = parseInt(req.params.age),
+			personData = req.body,
+			found = false;
+
+		let newList = persons.map(person => {
+			if(person.age === age) {
+				found = true;
+				return {
+					...person,
+					...personData
+				}
+			} else {
+				return person;
+			}
+		});
+
+		if(found) {
+			persons = newList;
+			return res.status(200).json({message: 'Users data successfully changed.'});		
+		} else {
+			return res.status(404).json({message: 'Users not found.'});
+		}
+	} catch(error) {
+		return res.status(500).json({message: 'There has been an internal server error.'});
+	}
+});
+
 app.delete('/persons/:ssn', (req, res) => {
 	try {
 		let ssn = parseInt(req.params.ssn), 
@@ -94,6 +124,15 @@ app.delete('/persons/:ssn', (req, res) => {
 			persons = newList;
 			return res.status(200).json({message: 'User successfully removed.'});		
 		}
+	} catch(error) {
+		return res.status(500).json({message: 'There has been an internal server error.'});
+	}
+});
+
+app.delete('/persons', (req, res) => {
+	try {
+		persons = [];
+		return res.status(200).json({message: 'Users successfully removed.'});
 	} catch(error) {
 		return res.status(500).json({message: 'There has been an internal server error.'});
 	}
